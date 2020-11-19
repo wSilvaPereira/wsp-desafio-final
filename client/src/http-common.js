7527b46c0788d8a5f38d5631f4cb89c51c6f1adf
William Pereira <william.pereira@escriba.com.br>
2020-11-19 14:12:56 -0300

    heroku

--- client/src/http-common.js
+++ client/src/http-common.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 //Define a URL base da origem para consumo do servico
 export default axios.create({
   // baseURL: 'https://wsp-grades-api.herokuapp.com',
-  baseURL: 'http://localhost:3000/',
+  baseURL: 'http://localhost:3001/',
   headers: {
     'Content-type': 'application/json',
   },
